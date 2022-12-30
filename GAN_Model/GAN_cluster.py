35c974cf12a6caf7fae318ae44e9fa8f76d78b93
BEAUTY\Niki <khoss.niklas@gmail.com>
2022-12-30 13:10:18 +0100

    converted notebook to scripts

--- GAN_Model/GAN_cluster.py
+++ GAN_Model/GAN_cluster.py
@@ -1,23 +1,19 @@
 # import necessary packages
-import tensorflow as tf
-import os
-import time
-import numpy as np
-import glob
-import matplotlib
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
-import PIL
-import imageio
-import random
-# import SimpleITK as sitk
-from IPython import display
-from IPython.display import Image
 
 from utils_gan import *
 
 # TODO: add commandline arguments (epoch, batch size, img output dir, model_name)
+BENIGN_PATH = '../dat/benign/'
+MALIGNANT_PATH = '../dat/malignant/'
+save_dir_benign = "./GAN_Images/benign/"
+save_dir_malignant = "./GAN_Images/malignant/"
+save_dir = save_dir_benign
+CHECKPOINT_PATH = './GAN_Images/training_checkpoints/'
+
+BATCH_SIZE = 16
+EPOCHS = 2
+noise_dim = 100
+num_examples_to_generate = 16
 
 
 def main():
@@ -30,7 +26,54 @@ def main():
 
     dataset = prepare_dataset(files_benign)
 
+    generator = Generator()
+    discriminator = Discriminator()
+    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    generator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
+    checkpoint = create_checkpoint(CHECKPOINT_PATH, generator_optimizer, discriminator_optimizer, generator,
+                                   discriminator)
+    checkpoint_prefix = os.path.join(CHECKPOINT_PATH, "ckpt")
+
+    random_vector_for_generation = tf.random.normal([num_examples_to_generate, noise_dim])
+
+    # Training
+    for epoch in range(EPOCHS):
+        start = time.time()
+
+        for images in dataset:
+            noise = tf.random.normal([BATCH_SIZE, noise_dim])
+
+            with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+                generated_images = generator(noise, training=True)
+
+                real_output = discriminator(images, training=True)
+                generated_output = discriminator(generated_images, training=True)
+
+                gen_loss = generator_loss(generated_output)
+                disc_loss = discriminator_loss(real_output, generated_output)
+
+            gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+            gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+            generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+            discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+        display.clear_output(wait=True)
+
+        generate_and_save_images(generator, epoch + 1, random_vector_for_generation, save_dir)
+
+        # saving (checkpoint) the model every 15 epochs
+        if (epoch + 1) % 15 == 0:  # das auf 15 ändern
+
+            # checkpoint.save(file_prefix = checkpoint_prefix)
+            checkpoint.save(file_prefix=checkpoint_prefix)
+
+        print('Time taken for epoch {} is {} sec'.format(epoch + 1, time.time() - start))
+
+    # generating after the final epoch
+    # display.clear_output(wait = True)
+    generate_and_save_images(generator, epoch, random_vector_for_generation, save_dir)
 
 
 if __name__ == '__main__':
--- GAN_Model/utils_gan.py
+++ GAN_Model/utils_gan.py
@@ -1,24 +1,18 @@
 # import necessary packages
-import tensorflow as tf
 import os
-import time
-import numpy as np
-import glob
-import matplotlib
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-
-import PIL
-import imageio
 import random
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
 # import SimpleITK as sitk
-from IPython import display
-from IPython.display import Image
 
 
 # global vars
-BENIGN_PATH = '../Dataset/benign/'
-MALIGNANT_PATH = '../Dataset/malignant/'
+
+
+# This method returns a helper function to compute cross entropy loss
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
 
 def get_all_images(path):
@@ -52,6 +46,138 @@ def prepare_dataset(files: list):
     random.shuffle(files)
     dataset = tf.data.Dataset.from_tensor_slices((files))
     dataset = dataset.shuffle(10000)
-    dataset = dataset.map(resize_image, num_parallel_calls = 16)
-    dataset = dataset.map(normalize_images, num_parallel_calls = 16)
+    dataset = dataset.map(resize_image, num_parallel_calls=16)
+    dataset = dataset.map(normalize_images, num_parallel_calls=16)
     return dataset
+
+
+# loss function discriminator
+def discriminator_loss(real_output, generated_output):
+    # [1,1,...,1] with real output since it is true and we want
+    # our generated examples to look like it
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    # [0,0,...,0] with generated images since they are fake
+    generated_loss = cross_entropy(tf.zeros_like(generated_output), generated_output)
+    total_loss = real_loss + generated_loss
+
+    return total_loss
+
+
+# loss function discriminator generator
+def generator_loss(generated_output):
+    return cross_entropy(tf.ones_like(generated_output), generated_output)
+
+
+def generate_and_save_images(model, epoch, test_input, imsave_dir):
+    # make sure the training parameter is set to False because we
+    # don't want to train the batchnorm layer when doing inference.
+    predictions = model(test_input, training=False)
+
+    fig = plt.figure(figsize=(16, 16))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i + 1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
+
+    # tight_layout minimizes the overlap between 2 sub-plots
+    plt.tight_layout()
+    plt.savefig(os.path.join(imsave_dir, 'image_at_epoch_{:04d}.png'.format(epoch)))
+    # plt.show()
+
+
+def create_checkpoint(cp_path, gen_opt, disc_opt, gen, disc):
+    checkpoint_prefix = os.path.join(cp_path, "ckpt")
+    checkpoint = tf.train.Checkpoint(generator_optimizer=gen_opt,
+                                     discriminator_optimizer=disc_opt,
+                                     generator=gen,
+                                     discriminator=disc)
+
+
+# create generator class
+class Generator(tf.keras.Model):
+
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.fc1 = tf.keras.layers.Dense(8 * 8 * 256, use_bias=False)
+        self.batchnorm1 = tf.keras.layers.BatchNormalization()
+
+        self.conv1 = tf.keras.layers.Conv2DTranspose(256 * 8, (4, 4), strides=(1, 1), padding='same', use_bias=False)
+        self.batchnorm2 = tf.keras.layers.BatchNormalization()
+
+        self.conv2 = tf.keras.layers.Conv2DTranspose(256 * 4, (4, 4), strides=(2, 2), padding='same', use_bias=False)
+        self.batchnorm3 = tf.keras.layers.BatchNormalization()
+
+        self.conv3 = tf.keras.layers.Conv2DTranspose(256 * 2, (4, 4), strides=(2, 2), padding='same', use_bias=False)
+        self.batchnorm4 = tf.keras.layers.BatchNormalization()
+
+        self.conv4 = tf.keras.layers.Conv2DTranspose(256 * 1, (4, 4), strides=(2, 2), padding='same', use_bias=False)
+        self.batchnorm5 = tf.keras.layers.BatchNormalization()
+
+        self.conv5 = tf.keras.layers.Conv2DTranspose(1, (4, 4), strides=(1, 1), padding='same', use_bias=False)
+
+    def call(self, x, training=True):
+        x = self.fc1(x)
+        x = self.batchnorm1(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = tf.reshape(x, shape=(-1, 8, 8, 256))
+
+        x = self.conv1(x)
+        x = self.batchnorm2(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = self.conv2(x)
+        x = self.batchnorm3(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = self.conv3(x)
+        x = self.batchnorm4(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = self.conv4(x)
+        x = self.batchnorm5(x, training=training)
+        x = tf.nn.relu(x)
+
+        x = tf.nn.tanh(self.conv5(x))
+
+        return x
+
+
+# create discriminator class
+class Discriminator(tf.keras.Model):
+
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(256, (4, 4), strides=(2, 2), padding='same')
+        self.conv2 = tf.keras.layers.Conv2D(256 * 2, (4, 4), strides=(2, 2), padding='same')
+        self.batchnorm2 = tf.keras.layers.BatchNormalization()
+        self.conv3 = tf.keras.layers.Conv2D(256 * 4, (4, 4), strides=(2, 2), padding='same')
+        self.batchnorm3 = tf.keras.layers.BatchNormalization()
+        self.conv4 = tf.keras.layers.Conv2D(256 * 8, (4, 4), strides=(2, 2), padding='same')
+        self.batchnorm4 = tf.keras.layers.BatchNormalization()
+        self.conv5 = tf.keras.layers.Conv2D(1, (4, 4), strides=(1, 1), padding='valid')
+
+        self.dropout = tf.keras.layers.Dropout(0.3)
+        self.flatten = tf.keras.layers.Flatten()
+        self.fc1 = tf.keras.layers.Dense(1)
+
+    def call(self, x, training=True):
+        x = tf.nn.leaky_relu(self.conv1(x))
+
+        x = self.conv2(x)
+        x = self.batchnorm2(x, training=training)
+        x = tf.nn.leaky_relu(x)
+
+        x = self.conv3(x)
+        x = self.batchnorm3(x, training=training)
+        x = tf.nn.leaky_relu(x)
+
+        x = self.conv4(x)
+        x = self.batchnorm4(x, training=training)
+        x = tf.nn.leaky_relu(x)
+
+        x = self.conv5(x)
+        x = self.fc1(x)
+
+        return x
